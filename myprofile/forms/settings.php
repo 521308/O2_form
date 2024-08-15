c7433ef42969757fe0377fe9dace260600c87f50
iron <68776979+521308@users.noreply.github.com>
2024-08-15 14:50:16 +0300

    Update settings.php

--- myprofile/forms/settings.php
+++ myprofile/forms/settings.php
@@ -4,13 +4,13 @@ return [
 	'mail_settings' => [               
 	    'host'       => 'smtp.gmail.com',                   
 	    'auth'   => true,                 
-	    'username'   => 'aelbert.kaitmazov@gmail.com',                    
-	    'password'  => 'lwnx iyej bdze tjdl',                               
+	    'username'   => '',                    
+	    'password'  => '',                               
 	    'secure' => "ssl",            
 	    'port'       => 465,
 	    'charset'    => 'UTF-8',
 	    'fromUsername' => 'Письмо с формы', 
-		'fromEmail' =>  'aelbert.kaitmazov@gmail.com',
+		'fromEmail' =>  '',
 		'is_html' => true,
 	],
 ];
